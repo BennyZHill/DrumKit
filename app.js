4c2ce10006e2ab05c544d7f9742f8d9669e151cd
BennyZHill <ben.h@builtbypixel.com>
2021-07-16 15:22:58 +0100

    commit

--- app.js
+++ app.js
@@ -12,39 +12,39 @@ window.addEventListener("keydown", function(event) {
 
     switch (event.which){
         case 49:     
-            let boom = new Audio("sounds/boom.wav");
+            let boom = new Audio("boom.wav");
             boom.play();
             break;
         case 50:    
-            let clap = new Audio("sounds/clap.wav");
+            let clap = new Audio("clap.wav");
             clap.play();
             break;
         case 51:        
-            let hihat = new Audio("sounds/hihat.wav");
+            let hihat = new Audio("hihat.wav");
             hihat.play();
             break;
         case 52:         
-            let kick = new Audio("sounds/kick.wav");
+            let kick = new Audio("kick.wav");
             kick.play();
             break;
         case 53:         
-            let openhat = new Audio("sounds/openhat.wav");
+            let openhat = new Audio("openhat.wav");
             openhat.play();
             break;
         case 54:        
-            let ride = new Audio("sounds/ride.wav");
+            let ride = new Audio("ride.wav");
             ride.play();
             break;
         case 55:        
-            let snare = new Audio("sounds/snare.wav");
+            let snare = new Audio("snare.wav");
             snare.play();
             break;
         case 56:        
-            let tink = new Audio("sounds/tink.wav");
+            let tink = new Audio("tink.wav");
             tink.play();
             break;
         case 57:         
-            let tom = new Audio("sounds/tom.wav");
+            let tom = new Audio("tom.wav");
             tom.play();
             break;
     }   
